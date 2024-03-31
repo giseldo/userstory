aea2b0ba2ba44fef9f9b8c39fc44aa7aa019afb5
Giseldo <giseldo@gmail.com>
2024-03-31 16:45:55 -0400

    Update 1_help.py

--- pages/1_help.py
+++ pages/1_help.py
@@ -2,23 +2,4 @@ import streamlit as st
 
 st.write("""
 
-Prezado colaborador,
-
-Você está sendo convidado(a) a participar de uma pesquisa de opinião intitulada: User Story Tutor, do doutorando  Giseldo da Silva Néo, orientado pelo professor Doutor José Antão Beltrão Moura, e discente do Doutorado em Ciência da Computação do Programa  do Programa de Pós-graduação em Ciência da Computação da UFCG. Ao todo, serão poucas perguntas às quais você poderá responder em cerca de 5 a 10 minutos.
-
-Link para o questionário [https://forms.gle/n5Ko39dHsPEjg3po9]
-
-A pesquisa se destina a coletar as percepções e sugestões dos participantes, quanto ao  User Story Tutor, cujo objetivo é  auxiliar os times ágeis na construção de User Stories melhores. A importância desta pesquisa se dá pela necessidade de obter dados de feedback capazes de averiguar se  o User Story Tutor atende ao objetivo proposto.
-
-O Desenvolvimento Ágil de Software (DAS) é uma disciplina que estuda um conjunto de comportamentos, processos, práticas e ferramentas utilizados para a criação de produtos (geralmente de, mas não limitados a, software) e sua subsequente disponibilização para os usuários finais. As metodologias e frameworks que fazem parte do conceito de desenvolvimento ágil (como o SCRUM, por exemplo) providenciam uma estrutura conceitual para conduzir projetos de engenharia de software.
-
-A User Story é uma sentença curta e simples sobre uma funcionalidade (escrita sob a perspectiva do usuário que a deseja), e são utilizadas para definir o escopo de um projeto de software que utiliza DAS. Ela é uma técnica de análise de requisitos e captura o "quem", "o quê" e "por quê" de uma forma concisa e simples, geralmente limitada em detalhes, de forma que possa ser escrita a mão em um pequeno cartão de notas de papel.
-
-Escrever uma User Story pode apresentar muita dificuldade. As User Stories podem ser muito rasas, e não apresentar o detalhe adequado para entender o resultado final esperado. As User Stories podem ser muito abrangentes, podendo ter o escopo de todo um módulo um sistema, o que também não é o mais adequado.  Elas também podem trazer dificuldades na hora do time ágil realizar suas estimativas. Escrever boas User Stories, que podem ser utilizadas para estimar esforço, aparece como um dos 5 mais importantes problemas em DAS em investigação realizado por Andrade (2021).
-
-As informações obtidas através da sua participação serão tratadas com os padrões profissionais de sigilo, não revelando a sua identidade. Os resultados desta pesquisa irão subsidiar uma tese de doutorado  e possivelmente textos que serão publicados em revistas científicas. Não havendo sua identificação em nenhuma publicação que possa resultar desta pesquisa.
-
-A sua participação é voluntária e você é livre para recusar-se a participar, retirar seu consentimento ou interromper sua participação nesta pesquisa a qualquer momento, através do endereço eletrônico (e-mail) giseldo@copin.ufcg.edu.br, sem que isso lhe traga qualquer penalidade, prejuízos ou perda de benefícios. Você também poderá ter os esclarecimentos sobre a pesquisa em qualquer momento que desejar. Também lhe serão fornecidos os esclarecimentos necessários sobre cada sessão abordada caso solicitado.
-         
-         
          """)
